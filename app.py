fe7503edcf710b2263831c605fe7d4ef8a718477
valerypoliakov <polyakov.v111@gmail.com>
2026-02-18 11:57:31 +0300

    Initialize database tables on startup

--- app.py
+++ app.py
@@ -150,3 +150,7 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
+# Инициализация БД для продакшена
+with app.app_context():
+    db.create_all()
